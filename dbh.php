5a8c9b7fa58a3ae10dd8227d654ef673e91e2550
limkaiying1998 <60492324+limkaiying1998@users.noreply.github.com>
2020-04-12 18:41:24 +0100

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -1,8 +1,8 @@
 <?php 
 $servername = "zpj83vpaccjer3ah.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-$dBUsername = "jlqcpwvog8lrjlre";
-$dBPassword = "f1mfmqrrmtrxhg1o";
-$dBName = "MyDatabase";
+$dBUsername = "pjs1ci1xo9m105uj";
+$dBPassword = "q6snk4oppywnxyjy";
+$dBName = "19159323";
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
